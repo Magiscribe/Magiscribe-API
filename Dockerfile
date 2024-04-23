a82ee3fda07dea4e0929683ab7e5dd10bf09f07e
Kyle Lierer <liererkt@gmail.com>
2024-04-23 17:15:31 -0500

    Added dockerfile and docker-compose.yml.

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Use an official Node runtime as a parent image
+FROM node:20-slim AS base
+ENV PNPM_HOME="/pnpm"
+ENV PATH="$PNPM_HOME:$PATH"
+RUN corepack enable
+
+WORKDIR /app
+
+# Builds production dependencies
+FROM base AS prod-deps
+COPY package.json pnpm-lock.yaml tsconfig.json .
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --prod
+
+# Builds the application
+FROM base AS build
+COPY . .
+RUN rm -rf node_modules
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install
+RUN pnpm run build
+
+# Runs the application
+FROM base
+COPY --from=prod-deps /app/package.json /app/pnpm-lock.yaml /app/tsconfig.json /app/
+COPY --from=prod-deps /app/node_modules /app/node_modules
+COPY --from=build /app/dist /app/dist
+
+# Set the environment variables unless they are already set
+ENV NODE_ENV=production
+ENV PORT=8000
+
+# Expose the port the app runs on
+EXPOSE $PORT
+
+CMD [ "pnpm", "start" ]
--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,4 +1,4 @@
-import { Agents, cleanJsonCode, executePrediction } from "utils/ai/system";
+import { Agents, cleanJsonCode, executePrediction } from "../utils/ai/system";
 
 const mutations = {
     addPrediction: async (_, { prompt, context }) => {
@@ -11,7 +11,7 @@ const mutations = {
         const json = JSON.parse(cleanedRes).processingSteps;
 
         // Add the context to each object
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const processedJson = json.map((obj: any) => {
             return {
                 ...obj,
@@ -25,8 +25,8 @@ const mutations = {
 
         return {
             prompt,
-            context: "the next big thing in tech will be",
-            result: "a new type of AI that can predict the future"
+            context,
+            result: JSON.stringify(processedJson),
         };
     }
 }
