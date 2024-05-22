702b235dabcbe7bccadb49031485993cf5f7e396
Kyle Lierer <liererkt@gmail.com>
2024-05-22 00:10:37 -0500

    Fixed package.json piping.

--- app/src/log.ts
+++ app/src/log.ts
@@ -3,6 +3,13 @@ import pino from 'pino';
 
 const log = pino({
   level: config.logLevel,
+  transport: config.environment == 'development' ? {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      ignore: 'hostname,pid',
+    },
+  } : undefined,
 });
 
 export default log;
