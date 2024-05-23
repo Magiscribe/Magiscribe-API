f4dd2e0b552d34a4f07d811495576a421ece1ef1
Kyle Lierer <liererkt@gmail.com>
2024-05-22 22:04:31 -0500

    CU-86b0gv6wg: Infrastructure environment fix.

--- infrastructure/bin/index.ts
+++ infrastructure/bin/index.ts
@@ -6,7 +6,7 @@ import config from '../bin/config';
 // Load environment variables aligning with the current environment.
 // Example: if the NODE_ENV environment variable is set to 'production', the .env.production file will be loaded.
 // Example: if the NODE_ENV environment variable is not set, the .env file will be loaded.
-const path = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+const path = `.env.dev`;
 dotenv.config({ path });
 
 const app = new App();
