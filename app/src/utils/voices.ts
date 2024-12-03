b9832702c741afba6ba6130bd8eab402332ead2a
Kyle Lierer <liererkt@gmail.com>
2024-12-02 20:34:46 -0600

    Added voice tags

--- app/src/utils/voices.ts
+++ app/src/utils/voices.ts
@@ -1,42 +1,44 @@
 export const VOICES = {
-  default: {
-    name: 'Default (Phoebe)',
-    voiceId: 'WtrVV2GXrArZVS0Sku5Y',
-    modelId: 'eleven_multilingual_v2',
-  },
   aria: {
-    name: 'Aria (Feminine)',
+    name: 'Aria',
+    tags: ['female', 'american', 'middle-aged'],
     voiceId: '9BWtsMINqrJLrRacOk9x',
     modelId: 'eleven_multilingual_v2',
   },
   brian: {
-    name: 'Brian (Masculine)',
+    name: 'Brian',
+    tags: ['male', 'american', 'middle-aged'],
     voiceId: 'nPczCjzI2devNBz1zQrb',
     modelId: 'eleven_multilingual_v2',
   },
+  charlotte: {
+    name: 'Charlotte',
+    tags: ['female', 'swedish', 'young'],
+    voiceId: 'XB0fDUnXU5powFXDhCwa',
+    modelId: 'eleven_multilingual_v2',
+  },
   chris: {
-    name: 'Chris (Masculine)',
+    name: 'Chris',
+    tags: ['male', 'american', 'middle-aged'],
     voiceId: 'iP95p4xoKVk53GoZ742B',
     modelId: 'eleven_multilingual_v2',
   },
   daniel: {
-    name: 'Daniel (Masculine)',
+    name: 'Daniel',
+    tags: ['male', 'british', 'middle-aged'],
     voiceId: 'onwK4e9ZLuTAKqWW03F9',
     modelId: 'eleven_multilingual_v2',
   },
   jessica: {
-    name: 'Jessica (Feminine)',
+    name: 'Jessica',
+    tags: ['female', 'british', 'sultry'],
     voiceId: 'flHkNRp1BlvT73UL6gyz',
     modelId: 'eleven_multilingual_v2',
   },
   phoebe: {
-    name: 'Phoebe (Feminine)',
+    name: 'Phoebe',
+    tags: ['female', 'british', 'posh'],
     voiceId: 'WtrVV2GXrArZVS0Sku5Y',
-    modelId: 'eleven_multilingual_v2',
-  },
-  charlotte: {
-    name: 'Charlotte (Feminine)',
-    voiceId: 'XB0fDUnXU5powFXDhCwa',
     modelId: 'eleven_multilingual_v2',
   },
 };
